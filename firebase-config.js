8c8e18ef089c72cc3fb16cc3fcea51339241d6d8
notAbhay321 <162017138+notAbhay321@users.noreply.github.com>
2024-11-22 00:41:50 +0530

    working version1

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCcPYZsXDCV3qaDwGzrsPgpxQMPrj5w8Eo",
+    authDomain: "chinmay-7c815.firebaseapp.com",
+    projectId: "chinmay-7c815",
+    storageBucket: "chinmay-7c815.firebasestorage.app",
+    messagingSenderId: "256933345252",
+    appId: "1:256933345252:web:741dc6fb12e12577e579f1",
+    measurementId: "G-KG74Q7MFXE"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
